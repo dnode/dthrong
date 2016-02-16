c157ac4579e74afe100596862752b707560ff9d4
Christoph Herrmann <developer@dragonprojects.de>
2016-02-16 21:16:11 +0100

    Upgrade to throng 2.0.1, no need to specify lifetime infinity anymore, its the default

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,5 @@
 let path = process.cwd() + '/' + require(process.cwd() + '/package.json').main;
 require('throng')(
   () => { require(path); },
-  {
-    workers: process.env.WEB_CONCURRENCY,
-    lifetime: Infinity
-  }
+  { workers: process.env.WEB_CONCURRENCY }
 );
